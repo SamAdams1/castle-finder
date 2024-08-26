29b4ecdd1ac9b11135b41356656185f98183dd10
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-08-25 20:22:49 -0400

    get points on map with popup

---
+++ getData/wikiAPI.py
@@ -0,0 +1,97 @@
+import requests
+import re
+import json
+import time
+# pageid: 8935083 
+
+# wikiEndpoint = "https://en.wikipedia.org/w/api.php?"
+wikiEndpoint = "https://de.wikipedia.org/w/api.php?"
+
+countriesParam = {
+  'origin': "*",
+  'format': "json",
+  'action': "query",
+  'prop': "iwlinks",
+  "iwprop": "url",
+  "list": "search",
+  "srsearch": "List of castles in Albania"
+}
+
+# params to get single coord
+geoLocParams = {
+  'origin': "*",
+  'format': "json",
+  'action': "query",
+  'prop': "coordinates",
+  'generator': "search",
+  'gsrsearch': "",
+  'gsrlimit': 1,
+}
+
+
+def appendParamsToUrl(parameters):
+    url = wikiEndpoint
+    for key in parameters.keys():
+      # if key == "gsrsearch" or key == "pageid":
+      #     parameters[key] = text.replace(" ", "_").replace("'", "")
+      url += f"{key}={parameters[key]}&"
+    # print(url)
+    return url
+
+def getCountryArticleName():
+  url = appendParamsToUrl(countriesParam)
+  response = requests.get(url)
+  data = response.json()
+  print(data)
+  
+# getCountryArticleName()
+
+def loopCountryCastleNames():
+  file = open("names.txt", "r",encoding="utf-8")
+  nameList = file.read().split("\n")
+  
+  withLoc = 0
+  noLoc = 0
+  
+  locations = {}
+  for castle in nameList:
+    try:
+      data =  searchSingleCastleCoords(castle)["query"]["pages"]
+      key = list(data.keys())[0]
+      latLon = [data[key]["coordinates"][0]["lat"], data[key]["coordinates"][0]["lon"]]
+      locations[castle] = latLon
+      # locations[castle.split(",")[0]] = latLon
+      withLoc += 1
+    except Exception as e:
+      print(e, castle)
+      noLoc += 1
+    time.sleep(0.0001)
+  
+  print(f"withLoc:{withLoc}, noloc: {noLoc}")
+  with open(f"b.json", "w") as outfile:
+    outfile.write(json.dumps(locations, indent=1))
+
+def searchSingleCastleCoords(castleName):
+  # append castlename to search keyword url
+  geoLocParams["gsrsearch"] = castleName.split(",")[0].replace(" ", "_")
+  url = appendParamsToUrl(geoLocParams)
+  
+  # api call
+  response = requests.get(url)
+  data = response.json()
+  
+  return data
+
+
+loopCountryCastleNames()
+# print(searchSingleCastleCoords("Second Battle of Wissembourg"))
+
+
+
+
+
+
+
+
+
+
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+import data from '../../../getData/austria.json';
+
+const castlesData: { [key: string]: number[] } = data;
+
 @Component({
   selector: 'app-map',
   standalone: true,
@@ -12,6 +16,7 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {
   ngOnInit(): void {
     this.configMap();
+    this.printlatLon();
   }
 
   map: any;
@@ -19,11 +24,32 @@ export class MapComponent implements OnInit {
   configMap() {
     this.map = L.map('map', {
       center: [47.5162, 14.5501],
-      zoom: 5,
+      zoom: 7,
     });
+    // adds tiles to map
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
+
+    // add markers to map
+    const iconSize = 15;
+    Object.keys(castlesData).forEach((key) => {
+      const coords = castlesData[key] as L.LatLngTuple;
+      // console.log(coords);
+      L.marker(coords, {
+        icon: new L.Icon({
+          iconUrl: '/red-dot.png',
+          iconSize: [iconSize, iconSize],
+          iconAnchor: [iconSize / 2, iconSize / 2],
+        }),
+      })
+        .addTo(this.map)
+        .bindPopup(`<h1>${key}</h1>`);
+    });
+  }
+
+  printlatLon() {
+    console.log(castlesData);
   }
 }
