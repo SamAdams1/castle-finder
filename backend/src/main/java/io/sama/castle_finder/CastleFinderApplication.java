a4c967eb62b68486eccdecaa5dea6d3f47c0231a
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-08-27 16:39:23 -0400

    backend

---
+++ backend/src/main/java/io/sama/castle_finder/CastleFinderApplication.java
@@ -0,0 +1,13 @@
+package io.sama.castle_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleFinderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/sama/castle_finder/CastleFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sama.castle_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
