009c6c749f8e36507c1e43c77bab5989e93eaff8
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-08-29 08:23:13 -0400

    change list component to follow new data schema

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,26 +1,31 @@
 import { Component } from '@angular/core';
 import { CastleService } from '../map/castles.service';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
 export class ListComponent {
-  castlesData;
-  castlesKeys;
+  data: Promise<[{ id: number; name: String; latLon: number[] }]>;
+  // castlesKeys;
 
   constructor(service: CastleService) {
-    this.castlesData = service.getCastlesData();
+    // this.loadCastles(service)
+    this.data = service.getCastlesData();
 
-    this.castlesKeys = Object.keys(this.castlesData);
+    // this.castlesKeys = Object.keys(this.castlesData);
   }
 
   copyLatLon(latLon: number[]) {
     console.log(latLon);
     navigator.clipboard.writeText(`${latLon}`);
   }
+
+  // async loadCastles(service: CastleService) {
+  //   this.data = await service.getCastlesData()
+  // }
 }
